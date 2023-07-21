3b412c1f446193fc0a3938b742c362859dbb5bb1
donlino97 <chamas_hadi@hotmail.com>
2023-07-21 19:58:36 +0300

    last version

--- app/firebase.js
+++ app/firebase.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4OvK8fea_T995ys4DWPH0ACyJXaIZgSY",
-  authDomain: "connect-react-7556c.firebaseapp.com",
-  projectId: "connect-react-7556c",
-  storageBucket: "connect-react-7556c.appspot.com",
-  messagingSenderId: "244293204702",
-  appId: "1:244293204702:web:2ed4757668689407d5204f",
-  measurementId: "G-XY6XN9GNE4"
+  apiKey: "AIzaSyCK_JMPJ58tb2xrVunjPTYuk8QH1dTsPSs",
+  authDomain: "learning-f4aba.firebaseapp.com",
+  projectId: "learning-f4aba",
+  storageBucket: "learning-f4aba.appspot.com",
+  messagingSenderId: "461965907872",
+  appId: "1:461965907872:web:6a525334387b2b98c5f6c5",
+  measurementId: "G-BVH6P9259T"
 };
 
 
